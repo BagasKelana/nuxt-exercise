78b2e9fb67b4982899dc2ed6016d1fa1b9037422
BagasKelana <hilalbagas@gmail.com>
2024-06-15 23:36:38 +0700

    fix(style) : darkmode and menu app navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -6,7 +6,7 @@
     app
     color="transparent"
     flat
-    height="75"
+    height="80"
   >
     <v-btn variant="text" class="mr-3" elevation="1" small>
       <v-icon icon="mdi-menu"> </v-icon>
@@ -29,43 +29,25 @@
       <v-icon class="mr-2" icon="mdi-magnify"></v-icon
     ></v-text-field>
 
-    <div class="mx-3" />
+    <div class="mx-2" />
 
-    <v-btn class="ml-2" min-width="0" to="/">
-      <v-icon>mdi-view-dashboard</v-icon>
+    <v-btn rounded class="ml-2" min-width="0">
+      <v-icon icon="mdi-tune-variant" />
     </v-btn>
 
-    <!-- <v-menu
-      bottom
-      left
-      offset-y
-      origin="top right"
-      transition="scale-transition"
-    >
-      <template v-slot:activator="{ attrs, on }">
-        <v-btn class="ml-2" min-width="0" text v-bind="attrs" v-on="on">
-          <v-badge color="red" overlap bordered>
-            <template v-slot:badge>
-              <span>5</span>
-            </template>
-
-            <v-icon>mdi-bell</v-icon>
-          </v-badge>
-        </v-btn>
-      </template>
-
-      <v-list :tile="false" nav>
-        <div>
-          <app-bar-item v-for="(n, i) in notifications" :key="`item-${i}`">
-            <v-list-item-title  />
-          </app-bar-item>
-        </div>
-      </v-list>
-    </v-menu> -->
-
-    <v-btn class="ml-2" min-width="0" to="">
-      <v-icon>mdi-account</v-icon>
+    <v-btn rounded class="ml-2" min-width="0">
+      <v-badge dot>
+        <v-icon icon="mdi-bell-outline"></v-icon>
+      </v-badge>
     </v-btn>
+
+    <v-avatar
+      class="ml-4 mr-2"
+      min-width="0"
+      variant="outlined"
+      size="small"
+      image="https://w7.pngwing.com/pngs/340/946/png-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes-thumbnail.png"
+    ></v-avatar>
   </v-app-bar>
 </template>
 
