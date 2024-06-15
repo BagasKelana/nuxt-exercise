d9844a6c29db1b76685ed3c06a8b98d451a38dae
BagasKelana <hilalbagas@gmail.com>
2024-06-15 23:35:38 +0700

    style: custom theme for dasboard

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -4,9 +4,29 @@ import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 
+const customDarkTheme = {
+  dark: true,
+  colors: {
+    background: '#191A1C',
+    surface: '#191A1B',
+    primary: '#3f51b5',
+    secondary: '#03dac6',
+    error: '#f44336',
+    info: '#2196F3',
+    success: '#4caf50',
+    warning: '#fb8c00',
+  },
+}
+
 export default defineNuxtPlugin((app) => {
   const vuetify = createVuetify({
     ssr: true,
+    theme: {
+      defaultTheme: 'customDarkTheme',
+      themes: {
+        customDarkTheme,
+      },
+    },
   })
   app.vueApp.use(vuetify)
 })
